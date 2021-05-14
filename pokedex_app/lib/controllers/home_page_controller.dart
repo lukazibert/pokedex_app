7a65626607fb5b0803814f51bb25b11f0a0c25ed
lukazibert <35371564+lukazibert@users.noreply.github.com>
2021-05-14 16:27:32 +0200

    Delete pokedex_app directory

--- pokedex_app/lib/controllers/home_page_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  PageController homeController = PageController();
-  var pokemonPageViewIndex = 0.obs;
-  var pokemonGritViewIndex = 0.obs;
-}
--- pokedex_app/lib/controllers/navigation_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-import 'package:get/get.dart';
-
-enum WidgetMarker { account, all, info, game }
-
-class NavigationController extends GetxController {
-  var selectedWidget = WidgetMarker.all.obs;
-}
